d76c3f1a8d641dffbd2faa0b33edaae06fb8c998
hhro <kwonsw212@gmail.com>
2021-08-30 15:05:51 +0900

    add: Admin._connect() at main.py

--- admin.py
+++ admin.py
@@ -1,6 +1,10 @@
 import pathlib
+import getpass
 import json
 import logging
+
+import paramiko
+
 from pathlib import Path
 
 cmds = {
@@ -8,17 +12,24 @@ cmds = {
         "repr": "Show servers",
         "method": "_show_servers"
     },
+    '2': {
+        "repr": "Quit",
+        "method": "_quit"
+    }
 }
 
 
 class Admin:
-    def __init__(self, user: str, key_p: pathlib.PosixPath, servers_p: pathlib.PosixPath) -> None:
+    def __init__(self, user: str, key_p: pathlib.PosixPath, servers_p: pathlib.PosixPath, is_encrypted: bool = False) -> None:
         self._user: str = user
         self._key_p: pathlib.PosixPath = key_p
         self._servers_p: pathlib.PosixPath = servers_p
 
+        self._is_encrypted: bool = is_encrypted
+
         self._servers: list[dict] = self._load_servers(self._servers_p)
         self._client = paramiko.SSHClient()
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     def _load_servers(self, servers_p: pathlib.PosixPath) -> list[dict[str:str]]:
         with open(servers_p) as servers_f:
@@ -27,34 +38,54 @@ class Admin:
                        for name in sorted(servers.keys())]
         return servers
 
-    def _menu(self):
-        print(f"{'[menu]':=^30}")
-        for cmd_idx in sorted(cmds.keys()):
-            print(f"{cmd_idx}. {cmds[cmd_idx]['repr']}")
-        print()
-        selected: str = input("Select > ")
-        print()
+    def _menu(self) -> None:
+        while True:
+            print(f"{'[menu]':=^30}")
+            for cmd_idx in sorted(cmds.keys()):
+                print(f"{cmd_idx}. {cmds[cmd_idx]['repr']}")
+            print()
+            selected: str = input("Select > ")
+            print()
 
-        print(f"{'['+cmds[selected]['repr']+']':=^30}")
-        method: function = getattr(self, cmds[selected]["method"])
-        method()
+            if cmds[selected]['method'] != '_quit':
+                print(f"{'['+cmds[selected]['repr']+']':=^30}")
 
-    def interacitve(self):
-        self._menu()
+            method: function = getattr(self, cmds[selected]["method"])
+            method()
 
-    def show_servers(self):
-        for idx, srv_name in enumerate(sorted(self._servers.keys())):
-            srv: dict = self._servers[srv_name]
+    def _connect(self, host: str, port: int) -> bool:
+        client = self._client
+        passphrase: str = None
 
-    def _show_servers(self):
+        if self._is_encrypted:
+            logging.info(f"Get passphrase of the f{self._key_p}")
+            passphrase = getpass.getpass(f"Passphrase for the private key: ")
+
+        logging.info(f"Try to connect to {host}:{port}")
+
+        try:
+            client.connect(hostname=host, port=port,
+                           username=self._user, key_filename=str(self._key_p), passphrase=passphrase)
+        except Exception as e:
+            raise e
+
+        logging.info(f"Connection to {host}:{port} has been established.")
+        return True
+
+    def _show_servers(self) -> bool:
         for idx, srv in enumerate(self._servers):
             print(f"[{idx+1}] {srv['name']}")
             print(f"Host: {srv['host']}")
             print(f"Port: {srv['port']}")
             print()
 
+    def _quit(self) -> None:
+        print("Bye!")
+        quit()
+
     def interacitve(self):
         self._menu()
+
 
 if __name__ == "__main__":
     Admin("3", "4", "servers.json").show_servers()
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import argparse
-import getpass
-from pathlib import Path
+import logging
+import paramiko
 
 import admin
+
+from pathlib import Path
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,29 +15,44 @@ if __name__ == "__main__":
                         help="username of the admin")
     parser.add_argument("-i", "--key", required=True,
                         help="path of the admin private key")
-    parser.add_argument("-p", "--phrase", action="store_true",
+    parser.add_argument("-p", "--encrypted", action="store_true", default=False,
                         help="passphrase is required")
     parser.add_argument(
         "-s", "--servers", default="./servers.json", help="path of the servers.json")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", default=False, help="verbose log messages"
+    )
+    parser.add_argument(
+        "-V", "--debug", action="store_true", default=False, help="more verbose log messages"
+    )
 
     args = parser.parse_args()
 
-    user = args.user
+    # username
+    user: str = args.user
 
-    # Private key
-    key_p = Path(args.key)
+    # private key
+    key_p: Path = Path(args.key)
 
     if not key_p.exists():
         raise FileNotFoundError(f"{key_p} not found")
 
-    if args.phrase:
-        phrase = getpass.getpass("Passphrase: ")
+    # private key is encrypted with the passphrase
+    is_encrypted: bool = args.encrypted
 
-    # Server
-    servers_p = Path(args.servers)
+    # server information
+    servers_p: Path = Path(args.servers)
 
     if not servers_p.exists():
         raise FileNotFoundError(f"{servers_p} not found")
 
-    master = admin.Admin(user, key_p, servers_p)
+    # logger
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+        paramiko.common.logging.basicConfig(level=logging.INFO)
+    elif args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+        paramiko.common.logging.basicConfig(level=logging.DEBUG)
+
+    master = admin.Admin(user, key_p, servers_p, is_encrypted)
     master.interacitve()
