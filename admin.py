6fd17f850fe6c0ce7d02ee8f10d38aeb9a23ac1c
seungwan.kwon <seungwan.kwon@prosys.kr>
2021-10-22 08:16:08 +0000

    update: health_check, connect

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-import pathlib
+import subprocess
 import getpass
 import json
 import logging
@@ -13,6 +13,10 @@ cmds = {
         "method": "_show_servers"
     },
     '2': {
+        "repr": "Spawn shell",
+        "method": "_spawn_shell"
+    },
+    '3': {
         "repr": "Quit",
         "method": "_quit"
     }
@@ -20,10 +24,10 @@ cmds = {
 
 
 class Admin:
-    def __init__(self, user: str, key_p: pathlib.PosixPath, servers_p: pathlib.PosixPath, is_encrypted: bool = False) -> None:
+    def __init__(self, user: str, key_p: Path, servers_p: Path, is_encrypted: bool = False) -> None:
         self._user: str = user
-        self._key_p: pathlib.PosixPath = key_p
-        self._servers_p: pathlib.PosixPath = servers_p
+        self._key_p: Path = key_p
+        self._servers_p: Path = servers_p
 
         self._is_encrypted: bool = is_encrypted
 
@@ -31,7 +35,7 @@ class Admin:
         self._client = paramiko.SSHClient()
         self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    def _load_servers(self, servers_p: pathlib.PosixPath) -> list[dict[str:str]]:
+    def _load_servers(self, servers_p: Path) -> list[dict[str:str]]:
         with open(servers_p) as servers_f:
             servers = json.load(servers_f)
             servers = [servers[name] | {'name': name}
@@ -51,7 +55,15 @@ class Admin:
                 print(f"{'['+cmds[selected]['repr']+']':=^30}")
 
             method: function = getattr(self, cmds[selected]["method"])
+
             method()
+
+    def _health_check(self, host: str):
+        try:
+            subprocess.check_output(f"ping -c 1 {host}".split())
+            return True
+        except subprocess.CalledProcessError as grepexc:
+            return False
 
     def _connect(self, host: str, port: int) -> bool:
         client = self._client
@@ -67,25 +79,36 @@ class Admin:
             client.connect(hostname=host, port=port,
                            username=self._user, key_filename=str(self._key_p), passphrase=passphrase)
         except Exception as e:
-            raise e
+            print(
+                f"Connect to {host}:{port} has been failed.\n"
+                f"Recommend to check whether private key requires passphrase.\n"
+                f"If you want to get more detail log, run with '--debug'"
+            )
+            return False
 
         logging.info(f"Connection to {host}:{port} has been established.")
         return True
 
-    def _show_servers(self) -> bool:
+    def _spawn_shell(self) -> None:
+        self._show_servers()
+        idx = int(input("Idx > "))-1
+
+        server = self._servers[idx]
+        self._connect(server["host"], server["port"])
+
+    def _show_servers(self) -> None:
         for idx, srv in enumerate(self._servers):
             print(f"[{idx+1}] {srv['name']}")
             print(f"Host: {srv['host']}")
             print(f"Port: {srv['port']}")
+            print(f"Alive: {'O' if self._health_check(srv['host']) else 'X'}")
             print()
 
     def _quit(self) -> None:
+        self._client.close()
+
         print("Bye!")
         quit()
 
     def interacitve(self):
         self._menu()
-
-
-if __name__ == "__main__":
-    Admin("3", "4", "servers.json").show_servers()
